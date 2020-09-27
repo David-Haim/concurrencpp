447788aca676d3a93706119f5d2d3b3e8e23ff0b
friendlyanon <friendlyanon@users.noreply.github.com>
2020-09-27 20:47:38 +0000

    Revert "Unused subproject"
    This reverts commit a5a4b15cc55e37c29f2e21bd5e52d9348f7ef6d2.

---
+++ sandbox/main.cpp
@@ -0,0 +1,13 @@
+#include "concurrencpp.h"
+
+#include <iostream>
+
+int main() {
+	concurrencpp::runtime runtime;
+	auto result = runtime.thread_pool_executor()->submit([] {
+		std::cout << "hello world" << std::endl;
+	});
+
+	result.get();
+	return 0;
+}
