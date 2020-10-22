c316c548376d2381f014ec41cd049803aa22785d
friendlyanon <friendlyanon@users.noreply.github.com>
2020-10-22 15:36:42 +0200

    Modify the imported target directly
    Turns out that targets can be modified from outside their defining
    scope, so instead of injecting a module that overwrites a command with
    potentially unwanted side-effects, the sanitizer flag can be applied
    directly.

--- cmake/concurrencppInjectTSAN.cmake
+++
@@ -1,9 +0,0 @@
-# Inject sanitizer flag as a build requirement
-#
-macro(add_library TARGET)
-  _add_library(${ARGV})
-
-  if("${TARGET}" STREQUAL "concurrencpp")
-    target_compile_options(concurrencpp PRIVATE -fsanitize=thread)
-  endif()
-endmacro()
