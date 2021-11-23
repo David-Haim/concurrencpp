ebcbf76ae36956ce0330c3b54fe6261e5324ab4e
David Haim <59602013+David-Haim@users.noreply.github.com>
2021-11-23 10:07:01 +0200

    Update cache_line.h

--- include/concurrencpp/threads/cache_line.h
+++ include/concurrencpp/threads/cache_line.h
@@ -1,9 +1,11 @@
 #ifndef CONCURRENCPP_CACHE_LINE_H
 #define CONCURRENCPP_CACHE_LINE_H
 
+#include "concurrencpp/platform_defs.h"
+
 #include <new>
 
-#ifdef __cpp_lib_hardware_interference_size
+#ifdef __cpp_lib_hardware_interference_size && !defined(CRCPP_MAC_OS)
 #    define CRCPP_CACHE_LINE_ALIGNMENT std::hardware_destructive_interference_size
 #else
 #    define CRCPP_CACHE_LINE_ALIGNMENT 64
