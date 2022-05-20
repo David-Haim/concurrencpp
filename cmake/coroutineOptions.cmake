18cb0ff78d228b37c2311af980e03172b07c14e5
Christoph Hausner <christoph_hausner@hotmail.de>
2022-05-20 22:54:05 +0200

    Make Threads::Threads a public dependency

--- cmake/coroutineOptions.cmake
+++ cmake/coroutineOptions.cmake
@@ -7,9 +7,6 @@ function(target_coroutine_options TARGET)
     return()
   endif()
 
-  find_package(Threads REQUIRED)
-  target_link_libraries(${TARGET} PRIVATE Threads::Threads)
-
   if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     target_compile_options(${TARGET} PUBLIC -stdlib=libc++ -fcoroutines-ts)
     target_link_options(${TARGET} PUBLIC -stdlib=libc++)
