36f696930bc9902649a4ed57d95d8903caaa3d63
chausner <15180557+chausner@users.noreply.github.com>
2022-05-22 21:04:08 +0200

    Make pthreads a public dependency, fix Windows CI (#78)
    * Make Threads::Threads a public dependency

--- cmake/setCiVars.cmake
+++ cmake/setCiVars.cmake
@@ -1,6 +1,6 @@
 if (os MATCHES "^windows")
   execute_process(
-    COMMAND "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat" && set
+    COMMAND "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat" && set
     OUTPUT_FILE environment_script_output.txt
   )
   file(STRINGS environment_script_output.txt output_lines)
