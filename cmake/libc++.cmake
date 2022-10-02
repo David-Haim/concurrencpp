c2057b46403cce2a093421c1f0beb6dec77eb77b
chausner <chausner@users.noreply.github.com>
2022-10-02 15:11:18 +0200

    Use string(APPEND)

--- cmake/libc++.cmake
+++ cmake/libc++.cmake
@@ -1,5 +1,5 @@
 # Specify this file as CMAKE_TOOLCHAIN_FILE when invoking CMake with Clang
 # to link to libc++ instead of libstdc++
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
+string(APPEND CMAKE_CXX_FLAGS " -stdlib=libc++")
+string(APPEND CMAKE_EXE_LINKER_FLAGS " -stdlib=libc++ -lc++abi")
