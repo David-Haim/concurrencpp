d8917c7a286408d20c77393d05ee139cc361bc04
David-Haim <dhayim1@gmail.com>
2023-07-09 10:52:00 +0300

    version bump

--- include/concurrencpp/runtime/constants.h
+++ include/concurrencpp/runtime/constants.h
@@ -12,7 +12,7 @@ namespace concurrencpp::details::consts {
 
     constexpr static unsigned int k_concurrencpp_version_major = 0;
     constexpr static unsigned int k_concurrencpp_version_minor = 1;
-    constexpr static unsigned int k_concurrencpp_version_revision = 6;
+    constexpr static unsigned int k_concurrencpp_version_revision = 7;
 }  // namespace concurrencpp::details::consts
 
 #endif
